46327fda11f442f3938de19caf08f4ee70316435
fadhlanrahim <fadhlanrh@gmail.com>
2020-10-19 09:57:37 +0700

    added comment line

--- Matrix 2x2 addition.py
+++ Matrix 2x2 addition.py
@@ -1,11 +1,11 @@
 #its shit right now, can only do 2 by 2 matrices
 #apparently, there is something called zip, and its actually the better way to loop over 2 lists
-#dont hardcode
+#dont hardcode, do list comprehension
 
 def add(matrix1, matrix2):
     combined = []
     for row1, row2 in zip(matrix1, matrix2):
-        combined.append([n + m for n, m in zip(row1, row2)] #these are list comprehension)
+        combined.append([n + m for n, m in zip(row1, row2)]) #these are list comprehension, could be reduced into just 1 line, but its harder to read
     return combined
 
 #try addding 2 2x2 matrices to test the code
