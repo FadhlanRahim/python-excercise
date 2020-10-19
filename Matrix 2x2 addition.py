acf0e45c0f53cc542feb9d23088399720f890710
fadhlanrahim <fadhlanrh@gmail.com>
2020-10-19 09:41:36 +0700

    deleted some old code lines and comment lines

--- Matrix 2x2 addition.py
+++ Matrix 2x2 addition.py
@@ -5,22 +5,12 @@
 def add(matrix1, matrix2):
     combined = []
     for row1, row2 in zip(matrix1, matrix2):
-        row = [n + m for n, m in zip(row1, row2)] # list comprehension
+        row = [n + m for n, m in zip(row1, row2)] #these are list comprehension
         #row = []                                   #these are not
         #for n, m in zip(row1, row2):
             #row.append(n + m)
         combined.append(row)
     return combined
-
-#def add(matrix1, matrix2):
-#    combined = []
-#    for i in range(len(matrix1)):
-#        row = []
-#        for j in range(len(matrix1[i])):
-#            row.append(matrix1[i][j] + matrix2[i][j])
-#        combined.append(row)
-#    return combined
-
 
 #try addding 2 2x2 matrices to test the code
 matrix1 = [[1, -2], [-3, 4]]
